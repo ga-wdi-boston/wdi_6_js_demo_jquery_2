06729bbf2a0552e9b35d5829447eabf94a004c55
David Fisher <tibbon@gmail.com>
2014-10-28 15:40:53 -0400

    Yet shorter

--- todo.js
+++ todo.js
@@ -1,29 +1,17 @@
-var TodoApp = {};
+var TodoApp = {
+  items: []
+};
 
 TodoApp.addItem = function() {
   var itemName = $(this).val();
+  $(this).val('');
   var item = new TodoItem(itemName);
-  TodoApp.renderItem(item);
   TodoApp.items.push(item);
 };
 
-TodoApp.renderItem = function(item) {
-  $('<div>').addClass('todo').html(item.name).appendTo(TodoApp.$list);
-};
+TodoApp.removeItem = function() {};
 
-TodoApp.removeItem = function() {
-
-};
-
-TodoApp.renderItems = function() {
-  for(var i = 0; i < this.items.length; i++) {
-    $('<div>').addClass('todo').html(this.items[i].name).appendTo(TodoApp.$list);
-  }
-};
-
-TodoApp.markCompleted = function() {
-
-};
+TodoApp.markCompleted = function() {};
 
 // Constructor Function
 // Gives me the ability to call:
@@ -32,9 +20,13 @@ TodoApp.markCompleted = function() {
 // Kinda like our initialize method in Ruby
 var TodoItem = function(name) {
   this.name = name;
+  this.render();
 };
 
 TodoItem.prototype = {
+  render: function() {
+    TodoApp.$list.append(this.toHTML());
+  },
   toHTML: function() {
     return $('<div>').addClass('todo').html(this.name);
   },
@@ -43,13 +35,7 @@ TodoItem.prototype = {
 }
 
 $(document).ready(function() {
-  TodoApp.items = [new TodoItem("Walk Dog"),
-                 new TodoItem("Goto 10"),
-                 new TodoItem("Goto 30")];
-
   TodoApp.$list = $('#todos');
-
   $('#new-todo').change(TodoApp.addItem)
-  TodoApp.renderItems();
 });
 
